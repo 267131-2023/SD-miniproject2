435a33b328e9659e96f11f1734562b2b1f7dc62d
KasiaFedoruk <127024200+KasiaFedoruk@users.noreply.github.com>
2024-04-29 15:31:38 +0200

    dodanie konstruktora, getterów i setterów w klasie Node

--- project2/Node.cpp
+++ project2/Node.cpp
@@ -1,3 +1,33 @@
-//
-// Created by kfedo on 29.04.2024.
-//
+#include "Node.h"
+
+Node::Node(int i_value, int i_priority) {
+    value = i_value;
+    priority = i_priority;
+    next = nullptr;
+}
+
+int Node::get_value() {
+    return value;
+}
+
+int Node::get_priority() {
+    return priority;
+}
+
+Node *Node::get_next() {
+    return next;
+}
+
+void Node::set_priority(int i_new_priority) {
+    priority = i_new_priority;
+}
+
+void Node::set_next(Node *next_node) {
+    next = next_node;
+}
+
+void Node::set_value(int i_new_value) {
+    value = i_new_value;
+}
+
+
