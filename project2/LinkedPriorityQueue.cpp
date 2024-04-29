49eb24432b9fab3dd18cf4c737dc824327b1a854
KasiaFedoruk <127024200+KasiaFedoruk@users.noreply.github.com>
2024-04-29 16:04:21 +0200

    Update LinkedPriorityQueue.cpp (insert function doesn't work)

--- project2/LinkedPriorityQueue.cpp
+++ project2/LinkedPriorityQueue.cpp
@@ -1,3 +1,42 @@
-//
-// Created by kfedo on 23.04.2024.
-//
+#include "PriorityQueue.h"
+
+PriorityQueue::PriorityQueue() {
+    head = nullptr;
+    size = 0;
+}
+
+void PriorityQueue::insert(Node *element, int i_priority) {
+    if(head == nullptr){
+        head = element;
+        head->set_priority(i_priority);
+        head->set_next(nullptr);
+        size++;
+    }
+
+    else{
+        Node* tmp_node = head;
+
+        if(element->get_priority() > tmp_node->get_priority()){
+            head = element;
+            element->set_next(tmp_node);
+        }
+
+        else{
+            while(tmp_node->get_next() != nullptr && element->get_priority() <= tmp_node->get_priority()){
+                tmp_node = tmp_node->get_next();
+            }
+
+            element->set_next(tmp_node->get_next());
+            tmp_node->set_next(element);
+            size++;
+        }
+    }
+}
+
+int PriorityQueue::return_size() {
+    return size;
+}
+
+Node *PriorityQueue::get_head() {
+    return head;
+}
