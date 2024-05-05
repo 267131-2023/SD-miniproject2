f2b5982e9127136ef9dcf35af8d4f8d4705355f5
KasiaFedoruk <127024200+KasiaFedoruk@users.noreply.github.com>
2024-05-05 21:29:21 +0200

    Update LinkedPriorityQueue.cpp
    added comments and removed spaces/repetitions

--- project2/LinkedPriorityQueue.cpp
+++ project2/LinkedPriorityQueue.cpp
@@ -4,42 +4,40 @@
 using namespace std;
 
 PriorityQueue::PriorityQueue() {
-    head = nullptr;
+    head = nullptr; 
     size = 0;
 }
 
 void PriorityQueue::insert(int element, int i_priority) {
     Node* node = new Node(element, i_priority);
-    if(head == nullptr){
+    if(head == nullptr){ // Jeśli lista jest pusta
         head = node;
     }
     else{
-
-        if(i_priority > head->get_priority()){
+        if(i_priority > head->get_priority()){ // Jeśli nowy węzeł ma wyższy priorytet niż głowa
             node->set_next(head);
             head = node;
         }
-
         else{
             Node* prev_node = head;
             Node* tmp_node = head->get_next();
+            // Znajdowanie poprawnej pozycji do wstawienia nowego węzła
             while(prev_node->get_next() != nullptr && i_priority <= tmp_node->get_priority()){
                 tmp_node = tmp_node->get_next();
                 prev_node = prev_node->get_next();
             }
             node->set_next(tmp_node);
             prev_node->set_next(node);
-            }
         }
+    }
     size++;
 }
 
-
 tuple<int, int> PriorityQueue::extract_max() {
-    if (head == nullptr) { // lista jest pusta
+    if (head == nullptr) { // Jeśli lista jest pusta
         cout << "Lista jest pusta" << endl;
     }
-    if (head->get_next() == nullptr) {// lista zawiera tylko jeden element
+    if (head->get_next() == nullptr) {
         int value = head -> get_value();
         int priority = head -> get_priority();
         delete head;
@@ -57,7 +55,7 @@ tuple<int, int> PriorityQueue::extract_max() {
 }
 
 tuple<int, int> PriorityQueue::peek() {
-    if (head == nullptr) { // lista jest pusta
+    if (head == nullptr) { // Jeśli lista jest pusta
         cout << "Lista jest pusta" << endl;
     } else{
         return make_tuple(head->get_value(), head->get_priority());
@@ -65,7 +63,7 @@ tuple<int, int> PriorityQueue::peek() {
 }
 
 void PriorityQueue::modify_key(int element, int i_priority) {
-    if (head == nullptr) {
+    if (head == nullptr) { // Jeśli lista jest pusta
         std::cout << "Kolejka jest pusta\n";
         return;
     }
@@ -73,25 +71,28 @@ void PriorityQueue::modify_key(int element, int i_priority) {
     Node* current_node = head;
     Node* prev_node = nullptr;
 
+    // Znajdowanie węzła o podanej wartości
     while (current_node != nullptr && current_node->get_value() != element) {
         prev_node = current_node;
         current_node = current_node->get_next();
     }
 
-    if (current_node == nullptr) {
+    if (current_node == nullptr) { // Jeśli węzeł nie istnieje
         std::cout << "Element " << element << " nie istnieje w kolejce\n";
         return;
     }
 
-    //decrease key
+    // Decrease key
     if (i_priority < current_node->get_priority()) {
         current_node->set_priority(i_priority);
         if (prev_node != nullptr) {
+            // Jeśli priorytet bieżącego węzła jest mniejszy niż jego poprzednika
             if (current_node->get_priority() < prev_node->get_priority()) {
                 prev_node->set_next(current_node->get_next());
                 Node* tmp_node = head;
                 Node* prev_temp = nullptr;
 
+                // Znajdowanie właściwej pozycji do wstawienia bieżącego węzła
                 while (tmp_node != nullptr && tmp_node->get_priority() > i_priority) {
                     prev_temp = tmp_node;
                     tmp_node = tmp_node->get_next();
@@ -100,9 +101,11 @@ void PriorityQueue::modify_key(int element, int i_priority) {
                 current_node->set_next(tmp_node);
             }
         } else {
+            // Jeśli bieżący węzeł jest głową
             Node* tmp_node = head->get_next();
             Node* prev_temp = head;
 
+            // Znajdowanie właściwej pozycji do wstawienia bieżącego węzła
             while (tmp_node != nullptr && tmp_node->get_priority() > i_priority) {
                 prev_temp = tmp_node;
                 tmp_node = tmp_node->get_next();
@@ -113,15 +116,17 @@ void PriorityQueue::modify_key(int element, int i_priority) {
             current_node->set_next(tmp_node);
         }
     }
-        // increase key
+        // Increase key
     else if (i_priority > current_node->get_priority()) {
         current_node->set_priority(i_priority);
         if (prev_node != nullptr) {
+            // Jeśli priorytet bieżącego węzła jest większy niż jego poprzednika
             if (current_node->get_priority() > prev_node->get_priority()) {
                 prev_node->set_next(current_node->get_next());
                 Node* tmp_node = head;
                 Node* prev_temp = nullptr;
 
+                // Znajdowanie właściwej pozycji do wstawienia bieżącego węzła
                 while (tmp_node != nullptr && tmp_node->get_priority() > i_priority) {
                     prev_temp = tmp_node;
                     tmp_node = tmp_node->get_next();
@@ -139,20 +144,14 @@ void PriorityQueue::modify_key(int element, int i_priority) {
     }
 }
 
-
-
 void PriorityQueue::display_queue() {
     Node* tmp = head;
-
     while(tmp != nullptr){
-        cout << "Value: " << tmp->get_value() << " Priority: " << tmp->get_priority() << endl;
+        cout << "Wartość: " << tmp->get_value() << " Priorytet: " << tmp->get_priority() << endl;
         tmp = tmp->get_next();
     }
 }
 
-void modify_key(Node* element, int i_priority){
-    
-}
 
 int PriorityQueue::return_size() {
     return size;
